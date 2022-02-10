e3f4cd2503c768349986f6fdb2d02d5f19878627
Виталий Сыресин <siresin@gmail.com>
2022-02-10 09:22:39 +0300

    10.02.2022 .2

--- cpp/homework7 _09.02.22 _task3.cpp
+++ cpp/homework7 _09.02.22 _task3.cpp
@@ -1,55 +1,73 @@
 // STEP _CPP _homework7 _09.02.22 _task3
-// Вывод симметричных звездочек
-// сделать с коэффициэнтом сжатия // int ratio = width / height;
-// Как вариант - с расстоянием от 'начала координат'
+// Вывод симметричной звездочки
 // 
+// С коэффициэнтом - float ratio = width / float(height);
+// - т.е. если стороны не равны - диагональ рисуется лесенкой
+
 
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 
 int main()
 {
 	setlocale(0, "");
-
+	srand(time(NULL));
 
 
 	const char MARK = 'O';
 	const char NOT_MARK = ' ';
 	const char WALL = '|';
 	const char LINE = '-';
-	const int ORIGIN = 0; // начало координат. Представим симметричную фигуру с центром в начале координат
 
-	int width = 15;
-	int height = 17;
-	int ratio = width / height;
+	int width;
+	int height;
+	char symb;
+
+	// Выбираем два случайных нечетных числа от 7 до 25
+	do
+	{
+		width = rand() % 20 + 6;
+	} while (width % 2 == 0);
 	
+	do
+	{
+		height = rand() % 20 + 6;
+	} while (height % 2 == 0);
+
+
+	float wRatio; // Коэффициент соотношения сторон
+	float hRatio; // Коэффициент соотношения сторон
+	if (width > height){
+		wRatio = width / float(height);
+		hRatio = 1;
+	}
+	else {
+		hRatio = height / float(width);
+		wRatio = 1;
+	}
+
+	// Представим звездочку как симметричную фигуру с центром в начале координат
+	// - поэтому границы задаются следущим образом
 	int maxWidth = width / 2;
 	int minWidth = -maxWidth;
 	int maxHeight = height / 2;
 	int minHeight = -maxHeight;
-	
-	char symb;
-
-
-	// это рабочий вариант
 	
 	for (int j = maxHeight; j >= minHeight; j--)
 	{
 		for (int i = minWidth; i <= maxWidth; i++)
 		{
 			symb = NOT_MARK;
-			//if (j == -i || j == i || j == 0 || i == 0) {
-			//abs(i)==abs(j) ||
-			if (abs(i)/ratio == abs(j) || i*j == 0) { ///
-				// Вариант с центром в начале координат - просто для простоты условий в данном If
+			if (int (abs(i) / wRatio) == int(abs(j) / hRatio) || i * j == 0) { 
 				symb = MARK;
 			}
-
 			std::cout << WALL << " " << symb << " ";
-			//std::cout << WALL << " " << i << "; " << j << " ";
-
 		}
 
+
+		// Код ниже - рисует крайнюю правую стенку и разделительную горизонтальную линию
 		std::cout << WALL << std::endl;
 		for (int k = 1; k <= width; k++)
 		{
@@ -58,33 +76,9 @@ int main()
 		std::cout << std::endl;
 	}
 
-	
+	std::cout << std::endl;
+	std::cout << "Размер звездочки: " << width << " x " << height << std::endl;
 
-
-	// это рабочий вариант
-	/*
-	for (int i = 1; i <= height; i++)
-	{
-		
-		for (int j = 1; j <= width; j++)
-		{
-			symb = NOT_MARK;
-			if (j == i || width-j+1 == i || j == width/2 + 1 || i == height/2 + 1){
-				symb = MARK;
-			}
-
-			std::cout << WALL << " " << symb << " ";
-
-		}
-		
-		std::cout << WALL << std::endl;
-		for (int k = 1; k <= height; k++)
-		{
-			std::cout << LINE << LINE << LINE << LINE;
-		}
-		std::cout << std::endl;
-	}
-	*/
 
 	return 0;
 
