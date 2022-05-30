15d0439814fd6236b5f98910d5135aa805aaf50e
Виталий Сыресин <siresin@gmail.com>
2022-05-30 13:24:31 +0300

    300522

--- cpp/homework28 _25.05.22.cpp
+++ cpp/homework28 _25.05.22.cpp
@@ -24,78 +24,107 @@ int getNWords(char* str) {
 }
 
 int getNumFromWord(char* word) {
-
-	//std::cout << word << "!";
-
-	char words[48][15] = {
-		"ноль ",
-		"один ",
-		"одина ",
-		"два ",
-		"две ",
-		"три ",
-		"четыре ",
-		"пять ",
-		"шесть ",
-		"семь ",
-		"восемь ",
-		"девять ",
-		"десять ",
-		"одиннадцать ",
-		"двенадцать ",
-		"тринадцать ",
-		"четырнадцать ",
-		"пятнадцать ",
-		"шестнадцать ",
-		"семнадцать ",
-		"восемнадцать ",
-		"девятнадцать ",
-		"двадцать ",
-		"тридцать ",
-		"сорок ",
-		"пятьдесят ",
-		"шестьдесят ",
-		"семдесят ",
-		"восемьдесят ",
-		"девяносто ",
-		"сто ",
-		"двести ",
-		"триста ",
-		"четыреста ",
-		"пятьсот ",
-		"шестьсот ",
-		"семьсот ",
-		"восемьсот ",
-		"девятьсот ",
-		"тысяча ",
-		"тысячи ",
-		"тысяч ",
-		"миллион ",
-		"миллиона ",
-		"миллионов ",
-		"миллиард ",
-		"миллиарда ",
-		"миллиардов "
-	};
-
-	int numbers[48] = {0,1,1,2,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,
-	30,40,50,60,70,80,90,100,200,300,400,500,600,700,800,900,1000,1000,1000,
-	1000000,1000000,1000000,1000000000,1000000000, 1000000000 };
-
-	/*char month[12][10] = {
-	"January", "February", "March",
-	"April", "May", "June",
-	"July", "August", "September",
-	"October", "November", "December"
-	};
-
-	char* month1[122] = { //так почему-то нельзя
-	"January", "February", "March",
-	"April", "May", "June",
-	"July", "August", "September",
-	"October", "November", "December"
-	};*/
 	
+	char words[48][15] = {
+		" ноль ",
+		" один ",
+		" одна ",
+		" два ",
+		" две ",
+		" три ",
+		" четыре ",
+		" пять ",
+		" шесть ",
+		" семь ",
+		" восемь ",
+		" девять ",
+		" десять ",
+		" одиннадцать ",
+		" двенадцать ",
+		" тринадцать ",
+		" четырнадцать ",
+		" пятнадцать ",
+		" шестнадцать ",
+		" семнадцать ",
+		" восемнадцать ",
+		" девятнадцать ",
+		" двадцать ",
+		" тридцать ",
+		" сорок ",
+		" пятьдесят ",
+		" шестьдесят ",
+		" семдесят ",
+		" восемьдесят ",
+		" девяносто ",
+		" сто ",
+		" двести ",
+		" триста ",
+		" четыреста ",
+		" пятьсот ",
+		" шестьсот ",
+		" семьсот ",
+		" восемьсот ",
+		" девятьсот ",
+		" тысяча ",
+		" тысячи ",
+		" тысяч ",
+		" миллион ",
+		" миллиона ",
+		" миллионов ",
+		" миллиард ",
+		" миллиарда ",
+		" миллиардов "
+	};
+
+	int numbers[48] = { 
+		0,
+		1,
+		1,
+		2,
+		2,
+		3,
+		4,
+		5,
+		6,
+		7,
+		8,
+		9,
+		10,
+		11,
+		12,
+		13,
+		14,
+		15,
+		16,
+		17,
+		18,
+		19,
+		20,
+		30,
+		40,
+		50,
+		60,
+		70,
+		80,
+		90,
+		100,
+		200,
+		300,
+		400,
+		500,
+		600,
+		700,
+		800,
+		900,
+		1000,
+		1000,
+		1000,
+		1000000,
+		1000000,
+		1000000,
+		1000000000,
+		1000000000,
+		1000000000 };
 
 	for (int i = 0; i < 48; i++) {
 		if (strstr(word, words[i])) return numbers[i];
@@ -109,8 +138,10 @@ int* getArrNum_OfNumber(char* str, int nWords) {
 	char* word = new char[10]; //10
 
 	int wordCounter = 0;
-	int symbCounter = 0;
+	int symbCounter = 1;
+	
 	for (int i = 0; i <= strlen(str); i++) {
+		word[0] = ' ';
 		if (str[i] != ' ' && str[i] != '\0') {
 			word[symbCounter] = str[i];
 			symbCounter++;
@@ -120,32 +151,52 @@ int* getArrNum_OfNumber(char* str, int nWords) {
 			word[symbCounter+1] = '\0';
 			arrNum_OfNumber[wordCounter] = getNumFromWord(word);
 			wordCounter++;
-			symbCounter = 0;
+			symbCounter = 1;
 		}
 	}
-	return arrNum_OfNumber; ////////////// одинаковые названия
+	return arrNum_OfNumber;
 }
 
-int* converStrToNum(char* str) {
+int converStrToNum(char* str) {
 	int nWords = getNWords(str);
 	int* arrNum_OfNumber = getArrNum_OfNumber(str, nWords);
+	int number = 0;
+	int numberPart = 0;
 
 	for (int i = 0; i < nWords; i++) {
-		std::cout << arrNum_OfNumber[i] << " ";
+		
+		if (arrNum_OfNumber[i]%1000000000 != 0 && arrNum_OfNumber[i] % 1000000 != 0 && arrNum_OfNumber[i] % 1000 != 0) {
+			numberPart += arrNum_OfNumber[i];
+		}
+		else {
+			numberPart *= arrNum_OfNumber[i];
+			number += numberPart;
+			numberPart = 0;
+		}
 	}
-
-	return arrNum_OfNumber;
+	number += numberPart;
+	
+	delete[]arrNum_OfNumber;
+	return number;
 }
 
 int main() {
 	setlocale(0, "");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
+	int number;
 
-	char* str = new char[200];
-	std::cout << "";
-	gets_s(str, 200);
-	converStrToNum(str);
+	do {
+		char* str = new char[250];
+		std::cout << "Введите число строкой(ноль - выход): ";
+		gets_s(str, 250);
+		number = converStrToNum(str);
+		std::cout << number;
+		std::cout << std::endl;
+		std::cout << std::endl;
+		delete[]str;
+	} while (number > 0);
+	
 
 	return 0;
 }
