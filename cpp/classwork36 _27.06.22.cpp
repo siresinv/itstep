4f2d8d759d0ad086c545384df32cf3b6246c8c62
Виталий Сыресин <siresin@gmail.com>
2022-06-27 21:26:06 +0300

    270622 - coworking1

---
+++ cpp/classwork36 _27.06.22.cpp
@@ -0,0 +1,193 @@
+﻿// STEP _CPP _classwork36 _27.06.22
+/*
+ООП НАЧАЛО
+
+this
+
+
+Задание самостоятельно на дом
+Создайте программу, имитирующую многоквартирный
+дом. Необходимо иметь классы “Человек”, “Квартира”,
+“Дом”. Класс “Квартира” содержит динамический массив
+объектов класса “Человек”. Класс “Дом” содержит массив
+объектов класса “Квартира”.
+Каждый из классов содержит переменные-члены и функции-члены, которые необходимы для предметной области
+класса. Обращаем ваше внимание, что память под строковые значения выделяется динамически. Например, для
+ФИО в классе “Человек”. Не забывайте обеспечить классы
+различными конструкторами (конструктор копирования
+обязателен), деструкторами. В main протестировать работу полученного набора классов.
+
+
+плюс предыдущую доделать и добавить к новому ДЗ конструктор копирования и другое из этого задания
+
+конст метод
+икспликт констр
+пергруз операт
+*/
+
+
+#include <iostream>
+#include <math.h>
+
+
+class Human {
+	char* name;
+
+public:
+
+};
+
+
+class Flat {
+	int num;
+	int nHuman;
+	Human* humans;
+public:
+	Flat(int size) :humans{ new Human[num] } {};
+};
+
+
+class House {
+	int nFlat;
+	Flat* flats;
+public:
+
+};
+
+
+
+
+
+
+
+//**************************************
+
+class DynArr {
+	int* arr;
+	int sizeArr;
+
+public:
+	DynArr(int uSizeArr) : arr{ new int[uSizeArr] }, sizeArr{ uSizeArr } {
+		std::cout << "Worked CONSTRUCTOR for " << this << std::endl;
+	}
+
+
+	DynArr(const DynArr& obj) : arr{ new int[obj.sizeArr] }, sizeArr{ obj.sizeArr } {
+		std::cout << "Worked COPY_CONSTRUCTOR for " << this << std::endl;
+		for (int i = 0; i < sizeArr; i++) {
+			arr[i] = obj.arr[i];
+		}
+	}
+
+
+
+	void print() {
+		for (int i = 0; i < sizeArr; i++) {
+			std::cout << arr[i] << " ";
+		}
+		std::cout << std::endl;
+	}
+
+	DynArr& randomize() {
+		for (int i = 0; i < sizeArr; i++) {
+			arr[i] = rand() % 10;
+		}
+		return *this;
+	}
+
+	~DynArr() {
+		if (arr) delete[] arr;
+		std::cout << "Worked DESTRUCTOR for " << this << std::endl;
+	}
+
+};
+
+
+
+
+
+//**************************************
+
+class Date {
+	int day, month, year;
+
+public:
+	Date(int d, int m, int y) : day{ d }, month{ m }, year{ y } {
+		std::cout << "Worked CONSTRUCTOR for " << this << std::endl;
+	}
+
+	Date() : Date(10, 11, 1970) {};
+
+	Date(const Date& obj): day{ obj.day }, month{ obj.month }, year{ obj.year }{
+		std::cout << "Worked COPY_CONSTRUCTOR for " << this << std::endl;
+	}
+
+	void print() {
+		std::cout << this->day << " " << this->month << " " << this->year << std::endl;
+	}
+
+	Date* setDay(int day) {
+		this->day = day;
+		return this;
+	}
+
+	Date& setMonth(int month) {
+		this->month = month;
+		return *this;
+	}
+
+	Date& setYear(int year) {
+		this->year = year;
+		return *this;
+	}
+
+	~Date() {
+		std::cout << "Worked DESTRUCTOR for " << this << std::endl;
+	};
+
+};
+
+
+int main()
+{
+	srand(time(NULL));
+
+
+
+
+	//**************************************
+
+	//Date d;
+	//d.print();
+
+	///*d.setDay(5);
+	//d.setMonth(12);
+	//d.setYear(1980);*/
+
+	//d.setDay(22)->setMonth(9).setYear(2000);
+
+	//Date d2{ 2,2, 1922 };
+	//d2.print();
+
+	//Date d3{ d };
+	//d3.print();
+
+	////std::cout << "__" << &d2 << "__";
+
+
+
+
+
+
+//**************************************
+
+	DynArr da1{ 10 }; // (10)
+	da1.randomize().print();
+
+	DynArr da2{ da1 };
+	da2.print();
+	
+	return 0;
+}
+
+
--- cpp/homework35 _22.06.22.cpp
+++ cpp/homework35 _22.06.22.cpp
@@ -11,19 +11,81 @@
 
 class String {
 	char* str;
+	static int strAmount;
+	int nChar;
 
 public:
-	String(const char* uStr, int lenStr = 80) : str{ new char [lenStr]} {
-		
+	
+
+	String(const char* uStr, int uLenStr) : str{ new char[uLenStr] } {
+		strcpy_s(str, uLenStr, uStr);
+		strAmount++;
+		nChar = uLenStr;
 	}
 
-	String(const char* uStr) : String(uStr, 80) {};
+	String(const char* uStr) : String(uStr, strlen(uStr) + 1) {};
+	String(int uLenStr) : String("\0", uLenStr + 1) {};
+	String() : String("\0", 21) {};
+
+	~String() {
+		//if (str) {
+			--strAmount;
+			delete[] str;
+		//}
+	}
+
+	void setStr() {
+		std::cout << "Enter string lenght " << nChar - 1 << ": ";
+		gets_s(str, nChar - 1);
+	}
+
+	void printStr() {
+		std::cout << str << " - lenght of this string/char_array: " << strlen(str) << "/" << nChar;
+		std::cout << std::endl;
+	}
+
+	static int getStrAmount() {
+		return strAmount;
+	}
+
 };
 
 
+int String::strAmount{ 0 };
+
 int main()
 {
-	
+	std::cout << "string amount: " << String::getStrAmount();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	String str1;
+	String str2("str2");
+	String str3(10);
+	str1.printStr();
+	str2.printStr();
+	str3.printStr();
+
+	std::cout << "string amount: " << String::getStrAmount();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
+	str1.setStr();
+	str1.printStr();
+	std::cout << std::endl;
+
+	str2.setStr();
+	str2.printStr();
+	std::cout << std::endl;
+
+	str3.setStr();
+	str3.printStr();
+	std::cout << std::endl;
+
+	std::cout << "string amount: " << String::getStrAmount();
+	std::cout << std::endl;
+	std::cout << std::endl;
+
 	return 0;
 }
 
