9c78c27bff2f15c13669c5651ed28bf7fc57363b
Виталий Сыресин <siresin@gmail.com>
2022-07-17 16:10:02 +0300

    170722 - h - 1

---
+++ cpp/selfwork43 _17.07.22 - templates.cpp
@@ -0,0 +1,44 @@
+﻿// STEP _CPP _selfwork43 _17.07.22 - templates
+/*
+
+*/
+
+
+#include <iostream>
+#include <string>
+
+
+template <class T>
+void getValue(std::string prompt, T& value) {
+
+	std::cout << prompt;
+	std::cin >> value;
+
+	while (std::cin.fail()) {
+		std::cin.clear();
+		std::cin.ignore(32767, '\n');
+		std::cout << "Error... try again" << std::endl;
+
+		std::cout << prompt;
+		std::cin >> value;
+	}
+
+	std::string endLine;
+	getline(std::cin, endLine);
+
+}
+
+
+int main()
+{
+
+	int number;
+	getValue("Enter number: ", number);
+
+
+	std::string name;
+	getValue("Enter name: ", name);
+
+	return 0;
+}
+
